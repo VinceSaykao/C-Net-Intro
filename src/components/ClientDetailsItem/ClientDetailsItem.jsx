6d701c6b8195d92f371aa1ef6ee414a946e25d51
Vince Saykao <90466599+VinceSaykao@users.noreply.github.com>
2022-06-11 21:51:52 -0500

    cleaned unsused imports

--- src/components/ClientDetailsItem/ClientDetailsItem.jsx
+++ src/components/ClientDetailsItem/ClientDetailsItem.jsx
@@ -7,17 +7,10 @@ import './ClientDetailsItem.scss';
 
 // MUI imports
 import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import TablePagination from '@mui/material/TablePagination';
+
 import EditIcon from '@mui/icons-material/Edit';
-import { Icon } from '@mui/material';
+
 
 export default function ClientDetailsItems({ timesheet }) {
 
